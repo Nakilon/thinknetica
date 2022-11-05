6b2c4c171b1fa0fa107fb599fbf6cf3ccab9383b
SupDex <symeton@gmail.com>
2022-11-06 01:03:47 +0300

    rewrite DirectLink class

--- autotests/day1/directlink/lib/directlink.rb
+++ autotests/day1/directlink/lib/directlink.rb
@@ -1,29 +1,72 @@
 require "fastimage"
+require "faraday"
 
-module DirectLink
+class DirectLink
+  attr_reader :link
+
   class ErrorBadLink < RuntimeError ; end
 
-  def self.imgur link
+  def initialize(link)
+    @link = link
+  end
+
+  def call
+    host = URI(link).host
+
+    unless host
+      raise ErrorBadLink.new(link)
+    end
+
+    case host.split(?.).last(2)
+    when %w{ imgur com }
+      imgur
+    when %w{ reddit com }
+      reddit
+    else
+      raise ErrorBadLink.new(link)
+    end
+  end
+
+  private
+
+  def imgur
     case link
     when /\Ahttps?:\/\/imgur\.com\/([a-zA-Z0-9]+)\z/
-      f = FastImage.new "#{link}.png"
+      f = FastImage.new("#{link}.png")
       ["#{link}.png", *f.size, f.type]
     else
       raise ErrorBadLink.new link
     end
   end
 
-  # def self.reddit link
-  # end
+  def reddit
+    case link
+    when /^https?:\/\/old\.reddit\.com([\w\/]*)$/
+      actual_json_link = link[".json"] ? link : "#{link}.json"
+      response = make_request(actual_json_link)
+      link_image = response.dig(0, "data", "children", 0, "data", "url_overridden_by_dest")
 
-end
+      unless link_image
+        raise ErrorBadLink.new(link)
+      end
 
-def DirectLink link
-  raise ::DirectLink::ErrorBadLink.new link unless host = URI(link).host
-  case host.split(?.).last(2)
-  when %w{ imgur com }
-    ::DirectLink.imgur link
-  else
-    raise ::DirectLink::ErrorBadLink.new link
+      f = FastImage.new(link_image)
+      [link_image, *f.size, f.type]
+    else
+      raise ErrorBadLink.new(link)
+    end
+  end
+
+  def make_request(actual_link)
+    conn = Faraday.new(url: actual_link) do |faraday|
+      faraday.response(:json)
+      faraday.request(:json)
+    end
+
+    response = conn.get("") do |request|
+      request.headers = { 'User-Agent' => 'Ruby' }
+    end
+
+    response.body
   end
 end
